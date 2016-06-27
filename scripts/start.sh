5c908abeb3b173347cebf7b56f9619b1e8e0a16b
Luca Capra <luca.capra@gmail.com>
2016-06-27 17:56:43 +0200

    added xvfb display

--- scripts/start.sh
+++ scripts/start.sh
@@ -14,13 +14,19 @@ if [ "${MODULE}" = 'all' ]; then
   echo "To start a single module use:\n $0 DeviceManager\|ProtocolManager\|BLE"
 fi
 
-export MAVEN_OPTS="-Djava.library.path=$DEPS"
+echo "Launching Dbus"
+Xvfb :1 -screen 0 1x1x8 &
+export DISPLAY=:1
+dbus-launch
+
+
+export MAVEN_OPTS="-Djava.library.path=$DEPS -DDISPLAY=$DISPLAY"
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$DEPS:$DEPS/lib
 
 if [ $MODULE = 'all' ] || [ $MODULE = 'BLE' ]; then
   ./scripts/stop.sh "protocol.BLE"
   cd iot.agile.protocol.BLE
-  mvn exec:java &
+  DISPLAY=:1 mvn exec:java &
   echo "Started AGILE BLE protocol"
   cd ../
 fi
@@ -28,7 +34,7 @@ fi
 if [ $MODULE = 'all' ] || [ $MODULE = 'ProtocolManager' ]; then
   ./scripts/stop.sh "protocolmanager"
   cd iot.agile.ProtocolManager
-  mvn exec:java &
+  DISPLAY=:1 mvn exec:java &
   echo "Started AGILE Protocol Manager"
   cd ..
 fi
@@ -36,7 +42,7 @@ fi
 if [ $MODULE = 'all' ] || [ $MODULE = 'DeviceManager' ]; then
   ./scripts/stop.sh "devicemanager"
   cd iot.agile.DeviceManager
-  mvn exec:java &
+  DISPLAY=:1 mvn exec:java &
   echo "Started AGILE Device Manager"
   cd ..
 fi
