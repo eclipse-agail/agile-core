d28d708c2cbc52bc4aeee184c852d626d63da6c8
Luca Capra <luca.capra@gmail.com>
2016-06-27 10:52:45 +0200

    fix realpath, added stop before start

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
-DEPS=`dirname ./deps`
+DEPS=`realpath ./deps`
 
-if [ ! -e "$DEPS" ] then
+if [ ! -e "$DEPS" ]; then
   echo "Installing dependencies"
   ./scripts/install-deps.sh
 fi
 
+echo "DEPS dir $DEPS"
+
 export MAVEN_OPTS="-Djava.library.path=$DEPS"
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$DEPS:$DEPS/lib
+
+./scripts/stop.sh
 
 cd iot.agile.protocol.BLE
 mvn exec:java &
