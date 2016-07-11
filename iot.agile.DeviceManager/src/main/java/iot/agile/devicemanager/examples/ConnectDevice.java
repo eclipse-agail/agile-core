b5f571df3147e3833886fcb05e167459169c8e59
dagmawi <doitdagi@gmail.com>
2016-07-11 21:07:53 +0200

    update in connect device client example

--- iot.agile.DeviceManager/src/main/java/iot/agile/devicemanager/examples/ConnectDevice.java
+++ iot.agile.DeviceManager/src/main/java/iot/agile/devicemanager/examples/ConnectDevice.java
@@ -35,56 +35,54 @@ import iot.agile.Device;
  */
 public class ConnectDevice {
 
-	protected final static Logger logger = LoggerFactory.getLogger(ConnectDevice.class);
+  protected final static Logger logger = LoggerFactory.getLogger(ConnectDevice.class);
 
-	/**
-	 * Bus name for AGILE BLE Device interface
-	 *  Default ID Sensor tag
-	 */
-	private static String deviceAgileID = "iot.agile.device.TISensorTag";
+  /**
+   * Bus name for AGILE BLE Device interface Default ID Sensor tag
+   */
+  private static String deviceAgileID = "iot.agile.device.TISensorTag";
 
-	/**
-	 * Bus path for AGILE BLE Device interface
-	 * Default Path : Sensor tag
-	 */
-	private static  String deviceAgileBusPath = "/iot/agile/Device/TISensorTag";
+  /**
+   * Bus path for AGILE BLE Device interface Default Path : Sensor tag
+   */
+  private static String deviceAgileBusPath = "/iot/agile/Device/TISensorTag";
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 
-		if (args.length == 1) {
-			if(isValidDeviceID(args[0])){
-				deviceAgileID = args[0].trim();
-				deviceAgileBusPath ="/iot/agile/Device/"+args[0].split(".")[3];
-			}else {
-				logger.info("Invalid device Agile ID, Using default value for TI-SensorTag:"+deviceAgileID);
-			}
-		}else{
-			logger.info("Invalid argument size, Using default values");
-		}
+    if (args.length == 1) {
+      if (isValidDeviceID(args[0])) {
+        deviceAgileID = args[0].trim();
+        deviceAgileBusPath = "/iot/agile/Device/" + args[0].split("\\.")[3];
+      } else {
+        logger.info("Invalid device Agile ID, Using default value for TI-SensorTag:" + deviceAgileID);
+      }
+    } else {
+      logger.info("Invalid argument size, Using default values");
+    }
 
-		try {
-			DBusConnection connection = DBusConnection.getConnection(DBusConnection.SESSION);
-			Device sensorTag = (Device) connection.getRemoteObject(deviceAgileID, deviceAgileBusPath,
-					Device.class);
-			if (sensorTag.Connect()) {
-				logger.info("Device Connected: {}", sensorTag.Name());
-			} else {
-				logger.info("Falied to connect : {}", sensorTag.Name());
-			}
-		} catch (DBusException e) {
-			e.printStackTrace();
-		}
+    try {
+      DBusConnection connection = DBusConnection.getConnection(DBusConnection.SESSION);
+      Device sensorTag = (Device) connection.getRemoteObject(deviceAgileID, deviceAgileBusPath, Device.class);
+      if (sensorTag.Connect()) {
+        logger.info("Device Connected: {}", sensorTag.Name());
+      } else {
+        logger.info("Falied to connect : {}", sensorTag.Name());
+      }
+    } catch (DBusException e) {
+      e.printStackTrace();
+    }
 
-	}
+  }
 
-	private static boolean isValidDeviceID(String deviceID) {
-		String[] idParts = deviceID.split(".");
-		if (idParts.length == 4) {
-			if ((idParts[0].equals("iot")) && (idParts[2].equals("agile")) && (idParts[3].equals("device"))) {
-				return true;
-			}
-		}
-		return false;
+  private static boolean isValidDeviceID(String deviceID) {
+    String[] idParts = deviceID.split("\\.");
 
-	}
+    if (idParts.length == 4) {
+      if ((idParts[0].equals("iot")) && (idParts[1].equals("agile")) && (idParts[2].equals("device"))) {
+        return true;
+      }
+    }
+    return false;
+
+  }
 }
