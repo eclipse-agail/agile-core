b07bf2bd62e524bca8c3d72d73d8fa26cc31ded7
curcuz <czkzcz.cmc@gmail.com>
2016-07-14 11:17:55 +0200

    make bluetooth work also on rpi3

--- app/scripts/start.sh
+++ app/scripts/start.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
+
+# Special procedure invoked only on rpi3 revision
+REV=`cat /proc/cmdline | awk -v RS=" " -F= '/boardrev/ { print $2 }'`
+
+if [ "$REV" = "0xa02082" ]
+  then
+    if ! /usr/bin/hciattach /dev/ttyAMA0 bcm43xx 921600 noflow -; then
+        /usr/bin/hciattach /dev/ttyAMA0 bcm43xx 921600 noflow - || true
+    fi
+fi
+# Enable Bluetooth
+hciconfig hci0 up || true
+
 PORT=80
 MODULE=${1:-all}
 DEPS=`realpath ./deps`
