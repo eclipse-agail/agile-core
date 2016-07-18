7326f9a917101fc498bca972b2f761c3d3bd131f
Luca Capra <luca.capra@gmail.com>
2016-07-18 14:11:19 +0200

    add @consumes @produces json annotations  for device manager

--- iot.agile.http/src/main/java/iot/agile/http/resource/DeviceManager.java
+++ iot.agile.http/src/main/java/iot/agile/http/resource/DeviceManager.java
@@ -26,12 +26,15 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import org.freedesktop.dbus.exceptions.DBusException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -41,6 +44,8 @@ import org.slf4j.LoggerFactory;
  * @author Luca Capra <lcapra@create-net.org>
  */
 @Path("/devices")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class DeviceManager {
   
   protected Logger logger = LoggerFactory.getLogger(DeviceManager.class);
