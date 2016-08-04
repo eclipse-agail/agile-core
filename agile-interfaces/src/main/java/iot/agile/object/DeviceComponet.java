4632c11133114e618804964166e4a62ec5eeaaa9
dagmawi <doitdagi@gmail.com>
2016-08-04 12:12:17 +0200

    Added JSON annotations for successful device registration via HTTP API

--- agile-interfaces/src/main/java/iot/agile/object/DeviceComponet.java
+++ agile-interfaces/src/main/java/iot/agile/object/DeviceComponet.java
@@ -3,13 +3,22 @@ package iot.agile.object;
 import org.freedesktop.dbus.Position;
 import org.freedesktop.dbus.Struct;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class DeviceComponet extends Struct {
   @Position(0)
+  @JsonProperty("id")
   public final String id;
+
   @Position(1)
+  @JsonProperty("unit")
   public final String unit;
- 
-  public DeviceComponet(String id, String unit){
+
+  @JsonCreator
+  public DeviceComponet(@JsonProperty("id") String id, @JsonProperty("unit") String unit) {
     this.id = id;
     this.unit = unit;
   }
--- agile-interfaces/src/main/java/iot/agile/object/DeviceDefinition.java
+++ agile-interfaces/src/main/java/iot/agile/object/DeviceDefinition.java
@@ -5,21 +5,32 @@ import java.util.List;
 import org.freedesktop.dbus.Position;
 import org.freedesktop.dbus.Struct;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class DeviceDefinition extends Struct {
   @Position(0)
+  @JsonProperty("id")
   public final String id;
   @Position(1)
+  @JsonProperty("name")
   public final String name;
   @Position(2)
+  @JsonProperty("protocol")
   public final String protocol;
   @Position(3)
+  @JsonProperty("path")
   public final String path;
   @Position(4)
+  @JsonProperty("streams")
   public final List<DeviceComponet> streams;
-  
-  
-  
-  public DeviceDefinition(String id, String protocol, String name, String path, List<DeviceComponet> streams) {
+
+  @JsonCreator
+  public DeviceDefinition(@JsonProperty("id") String id,@JsonProperty("name") String name, @JsonProperty("protocol") String protocol,
+       @JsonProperty("path") String path,
+      @JsonProperty("streams") List<DeviceComponet> streams) {
     this.id = id;
     this.protocol = protocol;
     this.name = name;
