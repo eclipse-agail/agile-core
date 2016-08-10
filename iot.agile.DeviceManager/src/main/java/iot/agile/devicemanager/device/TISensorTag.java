306dc7330277281908690a0829e9ad4864ae9f4a
dagmawi <doitdagi@gmail.com>
2016-08-10 11:41:51 +0200

    Implementation of humidity sensor for TISensorTag device

--- iot.agile.DeviceManager/src/main/java/iot/agile/devicemanager/device/TISensorTag.java
+++ iot.agile.DeviceManager/src/main/java/iot/agile/devicemanager/device/TISensorTag.java
@@ -10,15 +10,15 @@ import iot.agile.object.DeviceDefinition;
 import org.freedesktop.dbus.exceptions.DBusException;
 
 public class TISensorTag extends DeviceImp implements Device {
-    
-	private static final String SENSOR_NAME ="SENSOR_NAME";
-    
+
+	private static final String SENSOR_NAME = "SENSOR_NAME";
+
 	private static final String GATT_SERVICE = "GATT_SERVICE";
 
 	private static final String GATT_CHARACTERSTICS_VALUE = "VALUE_CHAR";
 
 	private static final String GATT_CHARACTERSTICS_CONFIG = "CONFIGURATION_CHAR";
-	
+
 	private static final String CONFIGURATION_VALUE = "CONFIGURATION_VALUE";
 	/**
 	 * TI Sensor Tag temperature service
@@ -44,7 +44,7 @@ public class TISensorTag extends DeviceImp implements Device {
 	 * TI Sensor Tag Humidity service
 	 */
 	private static final String HUMIDITY = "Humidity";
-
+	// F000AA20-0451-4000-B000-000000000000
 	private static final String HUMIDITY_GATT_SERVICE_UUID = "f000aa20-0451-4000-b000-000000000000";
 
 	private static final String HUMIDITY_GATT_CHARACTERSTICS_UUID = "f000aa21-0451-4000-b000-000000000000";
@@ -81,9 +81,6 @@ public class TISensorTag extends DeviceImp implements Device {
 
 	private static final String GYROSCOPE_GATT_CHARACTERSTICS_CONFIG_UUID = "f000aa52-0451-4000-b000-000000000000";
 
-	 
-	
-	
 	public TISensorTag(DeviceDefinition devicedefinition) throws DBusException {
 		super(devicedefinition);
 	}
@@ -96,15 +93,16 @@ public class TISensorTag extends DeviceImp implements Device {
 					try {
 						deviceProtocol.Write(deviceID, getProfile(sensorName.trim()));
 						/**
-						 * The default read data period (frequency) of most of sensor tag sensors
-						 * is 1000ms therefore the first data will be available to read after 1000ms
-						 * for these we call Read method after 1 second 
+						 * The default read data period (frequency) of most of
+						 * sensor tag sensors is 1000ms therefore the first data
+						 * will be available to read after 1000ms for these we
+						 * call Read method after 1 second
 						 */
-			              Thread.sleep(1010);
-			              String readValue = deviceProtocol.Read(deviceID, getProfile(sensorName.trim()));
-			              logger.info(readValue);
-			              return formatReading(sensorName, readValue);
- 					} catch (Exception e) {
+						Thread.sleep(1010);
+						String readValue = deviceProtocol.Read(deviceID, getProfile(sensorName.trim()));
+						logger.info(readValue);
+						return formatReading(sensorName, readValue);
+					} catch (Exception e) {
 						e.printStackTrace();
 					}
 				} else {
@@ -135,62 +133,74 @@ public class TISensorTag extends DeviceImp implements Device {
 		Map<String, String> profile = new HashMap<String, String>();
 		profile.put(SENSOR_NAME, sensorName);
 		if (sensorName.equals(TEMPERATURE)) {
-			byte[] configValue = {0x01};
+			byte[] configValue = { 0x01 };
 			profile.put(GATT_SERVICE, TEMP_GATT_SERVICE_UUID);
 			profile.put(GATT_CHARACTERSTICS_VALUE, TEMP_GATT_CHARACTERSTICS_UUID);
 			profile.put(GATT_CHARACTERSTICS_CONFIG, TEMP_GATT_CHARACTERSTICS_CONFIG_UUID);
 			profile.put(CONFIGURATION_VALUE, new String(configValue));
 		} else if (sensorName.equals(ACCELEROMETER)) {
-			profile.put(ACCELEROMETER + GATT_SERVICE, ACC_GATT_SERVICE_UUID);
-			profile.put(ACCELEROMETER + GATT_CHARACTERSTICS_VALUE, ACC_GATT_CHARACTERSTICS_UUID);
-			profile.put(ACCELEROMETER + GATT_CHARACTERSTICS_CONFIG, ACC_GATT_CHARACTERSTICS_CONFIG_UUID);
+			profile.put(GATT_SERVICE, ACC_GATT_SERVICE_UUID);
+			profile.put(GATT_CHARACTERSTICS_VALUE, ACC_GATT_CHARACTERSTICS_UUID);
+			profile.put(GATT_CHARACTERSTICS_CONFIG, ACC_GATT_CHARACTERSTICS_CONFIG_UUID);
 		} else if (sensorName.equals(HUMIDITY)) {
-			profile.put(HUMIDITY + GATT_SERVICE, HUMIDITY_GATT_SERVICE_UUID);
-			profile.put(HUMIDITY + GATT_CHARACTERSTICS_VALUE, HUMIDITY_GATT_CHARACTERSTICS_UUID);
-			profile.put(HUMIDITY + GATT_CHARACTERSTICS_CONFIG, HUMIDITY_GATT_CHARACTERSTICS_CONFIG_UUID);
+			byte[] configValue = { 0x01 };
+			profile.put(GATT_SERVICE, HUMIDITY_GATT_SERVICE_UUID);
+			profile.put(GATT_CHARACTERSTICS_VALUE, HUMIDITY_GATT_CHARACTERSTICS_UUID);
+			profile.put(GATT_CHARACTERSTICS_CONFIG, HUMIDITY_GATT_CHARACTERSTICS_CONFIG_UUID);
+			profile.put(CONFIGURATION_VALUE, new String(configValue));
 		} else if (sensorName.equals(BAROMETER)) {
-			profile.put(BAROMETER + GATT_SERVICE, MAGNETOMETER_GATT_SERVICE_UUID);
-			profile.put(BAROMETER + GATT_CHARACTERSTICS_VALUE, MAGNETOMETER_GATT_CHARACTERSTICS_UUID);
-			profile.put(BAROMETER + GATT_CHARACTERSTICS_CONFIG, MAGNETOMETER_GATT_CHARACTERSTICS_CONFIG_UUID);
+			profile.put(GATT_SERVICE, MAGNETOMETER_GATT_SERVICE_UUID);
+			profile.put(GATT_CHARACTERSTICS_VALUE, MAGNETOMETER_GATT_CHARACTERSTICS_UUID);
+			profile.put(GATT_CHARACTERSTICS_CONFIG, MAGNETOMETER_GATT_CHARACTERSTICS_CONFIG_UUID);
 		} else if (sensorName.equals(GYROSCOPE)) {
-			profile.put(GYROSCOPE + GATT_SERVICE, BAROMETER_GATT_SERVICE_UUID);
-			profile.put(GYROSCOPE + GATT_CHARACTERSTICS_VALUE, BAROMETER_GATT_CHARACTERSTICS_UUID);
-			profile.put(GYROSCOPE + GATT_CHARACTERSTICS_CONFIG, BAROMETER_GATT_CHARACTERSTICS_CONFIG_UUID);
+			profile.put(GATT_SERVICE, BAROMETER_GATT_SERVICE_UUID);
+			profile.put(GATT_CHARACTERSTICS_VALUE, BAROMETER_GATT_CHARACTERSTICS_UUID);
+			profile.put(GATT_CHARACTERSTICS_CONFIG, BAROMETER_GATT_CHARACTERSTICS_CONFIG_UUID);
 		} else if (sensorName.equals(MAGNETOMETER)) {
-			profile.put(MAGNETOMETER + GATT_SERVICE, GYROSCOPE_GATT_SERVICE_UUID);
-			profile.put(MAGNETOMETER + GATT_CHARACTERSTICS_VALUE, GYROSCOPE_GATT_CHARACTERSTICS_UUID);
-			profile.put(MAGNETOMETER + GATT_CHARACTERSTICS_CONFIG, GYROSCOPE_GATT_CHARACTERSTICS_CONFIG_UUID);
+			profile.put(GATT_SERVICE, GYROSCOPE_GATT_SERVICE_UUID);
+			profile.put(GATT_CHARACTERSTICS_VALUE, GYROSCOPE_GATT_CHARACTERSTICS_UUID);
+			profile.put(GATT_CHARACTERSTICS_CONFIG, GYROSCOPE_GATT_CHARACTERSTICS_CONFIG_UUID);
 		}
 		return profile;
 	}
-	
-	
-	private String formatReading(String sensorName, String readingValue){
-		String result;
-		if(sensorName.contains(TEMPERATURE)){
-			byte[] temp = readingValue.getBytes(StandardCharsets.ISO_8859_1);
-			/**
-			 * The temperature service returns the data in an encoded format
-			 * which can be found in the wiki. Convert the raw temperature
-			 * format to celsius and print it. Conversion for object
-			 * temperature depends on ambient according to wiki, but assume
-			 * result is good enough for our purposes without conversion.
-			 */
- 			int ambientTempRaw = (temp[2] & 0xff) | (temp[3] << 8);
 
- 			float ambientTempCelsius = convertCelsius(ambientTempRaw);
- 			result = Float.toString(ambientTempCelsius);
-		}else{
-			//TODO
-			result ="";
+	/**
+	 *
+	 * The sensor service returns the data in an encoded format which can
+	 * be found in the
+	 * wiki(http://processors.wiki.ti.com/index.php/SensorTag_User_Guide#
+	 * IR_Temperature_Sensor). 
+	 * Convert the raw sensor reading value format to human understandable value and
+	 * print it. 
+	 * @param sensorName
+	 *            Name of the sensor to read value from
+	 * @param readingValue
+	 *            the raw value read from the sensor
+	 * @return
+	 */
+	private String formatReading(String sensorName, String readingValue) {
+		String result;
+		if (sensorName.contains(TEMPERATURE)) {
+			byte[] temp = readingValue.getBytes(StandardCharsets.ISO_8859_1);
+			int ambientTempRaw = (temp[2] & 0xff) | (temp[3] << 8);
+			float ambientTempCelsius = convertCelsius(ambientTempRaw);
+			result = Float.toString(ambientTempCelsius);
+		} else if (sensorName.contains(HUMIDITY)) {
+			byte[] humidity = readingValue.getBytes(StandardCharsets.ISO_8859_1);
+			int lowerByte = Byte.toUnsignedInt(humidity[2]);
+			int upperByte = Byte.toUnsignedInt(humidity[3]);
+			int rawResult = (upperByte << 8) + (lowerByte & 0xff);
+			float hum = convertHumidity(rawResult);
+			result = Float.toString(hum);
+		} else {
+			// TODO
+			result = "";
 		}
-		
-		
+
 		return result;
-		
+
 	}
-	
-	
+
 	/**
 	 * Converts temperature into degree Celsius
 	 *
@@ -199,5 +209,16 @@ public class TISensorTag extends DeviceImp implements Device {
 	 */
 	private float convertCelsius(int raw) {
 		return raw / 128f;
+	}
+
+	/**
+	 * Formats humidity value according to SensorTag WIKI
+	 * 
+	 * @param raw
+	 * @return
+	 */
+	private float convertHumidity(int raw) {
+		int a = raw - (raw % 4);
+		return ((-6f) + 125f * (a / 65535f));
 	}
 }
