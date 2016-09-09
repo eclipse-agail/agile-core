423216ab901b0daa90c909980ddf322e0a181771
Csaba Kiraly <kiraly@fbk.eu>
2016-09-09 13:33:06 +0000

    add devicemanager-delete.py

---
+++ test/devicemanager-delete.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# --- Imports -----------
+import sys
+import time
+import dbus
+# -----------------------
+
+# DBus
+DM_BUS_NAME = "iot.agile.DeviceManager"
+DM_OBJ_PATH = "/iot/agile/DeviceManager"
+
+# --- Main program ------
+if __name__ == "__main__":
+
+   global device_manager
+
+   # DBus
+   session_bus = dbus.SessionBus()
+   dbusdm = session_bus.get_object(DM_BUS_NAME, DM_OBJ_PATH)
+   device_manager = dbus.Interface(dbusdm, dbus_interface=DM_BUS_NAME)
+
+   print (device_manager.Delete(sys.argv[1]))
