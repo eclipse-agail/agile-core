a593a7d951fe1de7a6ce00ca9b783b947a3db1aa
Csaba Kiraly <csaba.kiraly@gmail.com>
2016-09-20 12:01:51 +0200

    just spell checking

--- agile-interfaces/src/main/java/iot/agile/Protocol.java
+++ agile-interfaces/src/main/java/iot/agile/Protocol.java
@@ -58,7 +58,7 @@ public interface Protocol extends DBusInterface {
 	 *
 	 * @return the last record received by the read or subscribe operation
 	 */
-	@org.freedesktop.DBus.Description("Store the last record recived by read or subscribe")
+	@org.freedesktop.DBus.Description("Store the last record received by read or subscribe")
 	public byte[] Data();
 
 	/**
