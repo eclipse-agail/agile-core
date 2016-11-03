5f21c00e1f65a55825634f38194d629e697bd580
Csaba Kiraly <csaba.kiraly@gmail.com>
2016-11-03 16:41:52 +0100

    only use new register method in test scripts

--- test/http/registerDevice.sh
+++ test/http/registerDevice.sh
@@ -1,17 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 deviceID=\"$1\"
 echo $deviceID
 #Extract device from list of discovered devices
-device=$(curl -X GET --header 'Accept: application/json' 'http://localhost:8080/api/protocols/devices'  | jq '.[] | select(.id == '$deviceID')|{id: .id, name: .name, protocol: .protocol, status: .status }')
+device=$(curl -X GET --header 'Accept: application/json' 'http://localhost:8080/api/protocols/devices'  | jq '.[] | select(.id == '$deviceID')')
 
-#If SensorTag found Register it
-if [ ${#device[@]} -eq 0 ]; then
-  echo "SensorTag device not found"
+echo $device
+
+types=$(curl -X POST --header 'Content-Type: application/json'  --header 'Accept: application/json' 'http://localhost:8080/api/devices/typeof' -d "$device" | jq -r '@csv' )
+echo "Types: $types"
+echo ${#types[@]}
+
+if [ ${#types[@]} -ne 1 ]; then
+  echo "Cannot automatically determine Device type"
 else
-  address=$( echo "$device" | jq '.id' )
-  name=$( echo "$device" | jq '.name' )
-  protocol=$( echo "$device" | jq '.protocol' )
-
-  #Register sensor tag devices
-  curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d ' {"deviceId":"", "address": '$address' ,"name": '"$name"', "description":"", "protocol": '$protocol' , "path": "iot.agile.device.CCSensorTag","streams": [{"id": "Temperature", "unit": "C"}, {"id": "Optical", "unit": "Lux"}]}' 'http://localhost:8080/api/devices'
+  #Register devices
+  echo '{"overview": '"$device"' , "type": '"${types[0]}"'}'
+  curl  -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"overview": '"$device"' , "type": '"${types[0]}"'}' 'http://localhost:8080/api/devices'
 fi
+
+
