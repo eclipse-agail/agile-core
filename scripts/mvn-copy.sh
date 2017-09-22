89ed5711355d414340e72c832070b4ef132d0f05
Luca Capra <luca.capra@gmail.com>
2017-09-22 12:02:12 +0200

    cp

---
+++ scripts/mvn-copy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+ssh agilepi1 -c 'mkdir agilepi1:~/git/agile-core/iot.agile.ProtocolManager/target'
+ssh agilepi1 -c 'mkdir agilepi1:~/git/agile-core/iot.agile.DeviceManager/target'
+ssh agilepi1 -c 'mkdir agilepi1:~/git/agile-core/iot.agile.DeviceFactory/target'
+ssh agilepi1 -c 'mkdir agilepi1:~/git/agile-core/iot.agile.http/target'
+
+scp iot.agile.ProtocolManager/target/protocol-manager-1.0-jar-with-dependencies.jar  agilepi1:~/git/agile-core/iot.agile.ProtocolManager/target
+scp iot.agile.DeviceManager/target/device-manager-1.0-jar-with-dependencies.jar  agilepi1:~/git/agile-core/iot.agile.DeviceManager/target
+scp iot.agile.DeviceFactory/target/DeviceFactory-1.0-jar-with-dependencies.jar  agilepi1:~/git/agile-core/iot.agile.DeviceFactory/target
+scp iot.agile.http/target/http-1.0-jar-with-dependencies.jar  agilepi1:~/git/agile-core/iot.agile.http/target
