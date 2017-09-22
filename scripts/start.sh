d8732fbbba2dc58ae95e1b09f08c7051a1d58a18
Luca Capra <luca.capra@gmail.com>
2017-09-22 11:59:39 +0200

    rm dbus address set

--- scripts/start.sh
+++ scripts/start.sh
@@ -25,47 +25,6 @@ if [ "${MODULE}" = '' ]; then
   exit 1
 fi
 
-TOEXPORT=""
-
-if [ ! -z "$DISPLAY" ]; then
-  echo ">> DISPLAY available, reusing current display"
-else
-  export DISPLAY=:0
-  TOEXPORT="\n$TOEXPORT\nexport DISPLAY=$DISPLAY"
-fi
-
-ME=`whoami`
-
-if [ ! -z "$DBUS_SESSION_BUS_ADDRESS" ]; then
-  echo ">> DBUS_SESSION_BUS_ADDRESS available, reusing current instance"
-else
-
-  if [ `pgrep -U $ME dbus-daemon -c` -gt 0 ]; then
-
-    echo ">> DBus session available"
-
-    MID=`sed "s/\n//" /var/lib/dbus/machine-id`
-    DISPLAYID=`echo $DISPLAY | sed "s/://"`
-    SESSFILEPATH="/home/$ME/.dbus/session-bus/$MID-$DISPLAYID"
-
-    if [ -e $SESSFILEPATH ]; then
-      echo ">> Loading DBus session instance address from local file"
-      echo ">> Source: $SESSFILEPATH"
-      . "$SESSFILEPATH"
-    else
-      echo "Cannot get Dbus session address. Panic!"
-    fi
-
-  else
-    export `dbus-launch`
-    sleep 2
-    echo "++ Started a new DBus session instance"
-  fi
-
-fi
-
-TOEXPORT="\n$TOEXPORT\nexport DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS"
-
 if [ -z "$DBUS_SESSION_BUS_ADDRESS" ]; then
   echo "!! Cannot export DBUS_SESSION_BUS_ADDRESS. Exit"
   exit 1
