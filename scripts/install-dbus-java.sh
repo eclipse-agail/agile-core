781cb7b475b3b00df6a5701417808feee654ac48
Csaba Kiraly <csaba.kiraly@gmail.com>
2017-10-07 08:57:07 +0000

    use our own repository for dbus-java
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>

--- scripts/install-dbus-java.sh
+++ scripts/install-dbus-java.sh
@@ -29,7 +29,7 @@ rm libmatthew-java-$LMLIB.tar.gz
 #wget https://dbus.freedesktop.org/releases/dbus-java/dbus-java-$DBUSJAVA.tar.gz
 #tar -xzf dbus-java-$DBUSJAVA.tar.gz
 #rm dbus-java-$DBUSJAVA.tar.gz
-git clone https://github.com/jeanparpaillon/dbus-java.git dbus-java-$DBUSJAVA
+git clone https://github.com/Agile-IoT/dbus-java.git dbus-java-$DBUSJAVA
 ( cd dbus-java-$DBUSJAVA && git checkout $DBUSJAVA )
 
 mv dbus-java-$DBUSJAVA $BUILD
